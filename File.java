c30b8646cf0e5e4eceb8154c33b5176c969dea40
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2020-09-05 02:17:27 +0800

    Update File.java

--- File.java
+++ File.java
@@ -1,18 +1,15 @@
-import javafx.stage.*;
-
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.net.URI;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.nio.file.Files;
+import java.util.*;
 
+import javafx.stage.*;
 
-public class File extends java.io.File {
-    public PrintWriter out;
-    public Scanner in;
-    public FileOutputStream outstream;
+public class File extends java.io.File implements Cloneable, AutoCloseable {
+    public PrintWriter out = null;
+    public Scanner in = null;
+    public FileOutputStream outstream = null;
     private char type;
     private static FileChooser fc = new FileChooser();
     private static DirectoryChooser dc = new DirectoryChooser();
@@ -27,6 +24,7 @@ public class File extends java.io.File {
             XHTML = new FileChooser.ExtensionFilter("Extensible HyperText Markup Language (XHTML) Files", "*.xhtml"),
             PHTML = new FileChooser.ExtensionFilter("PHP HyperText Markup Language (PHTML) Files", "*.phtml"),
             XML = new FileChooser.ExtensionFilter("Extensible Markup Language (XML) Files", "*.xml"),
+            FXML = new FileChooser.ExtensionFilter("JavaFX XML Files", "*.fxml"),
             PHP = new FileChooser.ExtensionFilter("PHP Files", "*.php", "*.php5"),
             CSS = new FileChooser.ExtensionFilter("Cascading Style Sheets (CSS) Files", "*.css"),
             JS = new FileChooser.ExtensionFilter("JavaScript (JS) Files", "*.js", "*.jsm"),
@@ -38,6 +36,7 @@ public class File extends java.io.File {
             RUBY = new FileChooser.ExtensionFilter("Ruby Files", "*.rb", "*.rbw", "*.rake", "*.rbx"),
             C = new FileChooser.ExtensionFilter("C Files", "*.c", "*.h", "*.idc"),
             CPP = new FileChooser.ExtensionFilter("C++ Files", "*.cpp", "*.hpp", "*.c++", "*.h++", "*.cc", "*.hh", "*.cxx", "*.hxx", "*.C", "*.H", "*.cp", "*.cpp"),
+            CSHARP = new FileChooser.ExtensionFilter("C# Files", "*.cs"),
             INO = new FileChooser.ExtensionFilter("Arduino Files", "*.ino"),
             VB = new FileChooser.ExtensionFilter("Visual Basic Files", "*.vbs"),
             GO = new FileChooser.ExtensionFilter("Golang Files", "*.go"),
@@ -62,26 +61,25 @@ public class File extends java.io.File {
 
     public File(String filename, char type) {
         super(filename);
+        try {
+            if (!exists()) createNewFile();
+        } catch(IOException ex) {}
         this.type = type;
         if(!isDirectory()) {
-            if (type == 'r' || type == 'w' || type == 'a') {
+            if ((type == 'r' || type == 'w' || type == 'a') && Files.isReadable(toPath())) {
                 try {
                     in = new Scanner(this);
-                } catch (IOException e) {
-                    in = null;
-                }
+                } catch (IOException e) {}
             }
             if (type == 'r') {
                 outstream = null;
                 out = null;
-            } else if (type == 'w') {
+            } else if (type == 'w' && Files.isWritable(toPath())) {
                 try {
                     outstream = new FileOutputStream(filename);
                     out = new PrintWriter(outstream);
-                } catch (IOException e) {
-                    out = null;
-                }
-            } else if (type == 'a') {
+                } catch (IOException e) {}
+            } else if (type == 'a' && Files.isWritable(toPath())) {
                 try {
                     outstream = new FileOutputStream(filename, true);
                     out = new PrintWriter(outstream);
@@ -89,53 +87,29 @@ public class File extends java.io.File {
                     out = null;
                 }
             }
-        } else {
-            in = null;
-            out = null;
-            outstream = null;
         }
     }
 
-    public File() {
-        this('r');
-    }
+    public File() { this('r'); }
 
-    public File(char type) {
-        this(getFile(new Stage()), type);
-    }
+    public File(char type) { this(getFile(), type); }
 
-    public File(File file) {
-        this(file.getAbsolutePath());
-    }
+    public File(java.io.File file) { this(file.getAbsolutePath()); }
 
-    public File(java.io.File file) {
-        this(file.getAbsolutePath());
-    }
-
-    public File(File file, char type) {
-        this(file.getAbsolutePath(), type);
-    }
-
-    public File(java.io.File file, char type) {
-        this(file.getAbsolutePath(), type);
-    }
-
+    public File(java.io.File file, char type) { this(file.getAbsolutePath(), type); }
 
     public String getName() { return getAbsolutePath(); }
 
     public boolean rename(File dest) { return renameTo(dest); }
 
-    public File[] listFiles() {
-        return convert(listFiles());
-    }
+    public File[] listFiles() { return convert(listFiles()); }
 
-    public static File[] convert(java.io.File ...files) {
-        File[] rearr = new File[files.length];
-        for(int i = 0; i < files.length; i++) {
-            rearr[i] = new File(files[i]);
-        }
-        return rearr;
-    }
+    public String getRelativePath() { return new File(System.getProperty("user.dir")).relativize(this); }
+    public String getRelativePath(String directory) { return new File(directory).relativize(this); }
+    public String getRelativePath(File directory) { return directory.relativize(this); }
+    public String getRelativePath(java.io.File directory) { return directory.toURI().relativize(this.toURI()).getPath(); }
+
+    public static File[] convert(java.io.File ...files) { return convert(Arrays.asList(files)); }
 
     public static File[] convert(List<java.io.File> files) {
         File[] rearr = new File[files.size()];
@@ -155,78 +129,98 @@ public class File extends java.io.File {
         return convert(fc.showOpenMultipleDialog(stage));
     }
 
-    public static File getFile(Stage stage, FileChooser.ExtensionFilter...extensions) {
-        fc.getExtensionFilters().addAll(Arrays.asList(extensions));
+    public static File getFile() { return getFile(new Stage()); }
+    public static File[] getFiles() { return getFiles(new Stage()); }
+
+    public static File getFile(Stage stage, FileChooser.ExtensionFilter...extensions) { return getFile(stage, Arrays.asList(extensions)); }
+
+    public static File getFile(Stage stage, Collection<FileChooser.ExtensionFilter> extensions) {
+        fc.getExtensionFilters().addAll(extensions);
         File file = getFile(stage);
         fc.getExtensionFilters().clear();
         return file;
     }
 
-    public static File[] getFiles(Stage stage, FileChooser.ExtensionFilter...extensions) {
-        fc.getExtensionFilters().addAll(Arrays.asList(extensions));
+    public static File getFile(FileChooser.ExtensionFilter...extensions) { return getFile(new Stage(), extensions); }
+    public static File getFile(Collection<FileChooser.ExtensionFilter> extensions) { return getFile(new Stage(), extensions); }
+
+    public static File[] getFiles(Stage stage, FileChooser.ExtensionFilter...extensions) { return getFiles(stage, Arrays.asList(extensions)); }
+
+    public static File[] getFiles(Stage stage, Collection<FileChooser.ExtensionFilter> extensions) {
+        fc.getExtensionFilters().addAll(extensions);
         File[] files = getFiles(stage);
         fc.getExtensionFilters().clear();
         return files;
     }
 
-    public static File getPNG(Stage stage) {
-        return getFile(stage, PNG);
-    }
+    public static File[] getFiles(FileChooser.ExtensionFilter...extensions) { return getFiles(new Stage(), extensions); }
+    public static File[] getFiles(Collection<FileChooser.ExtensionFilter> extensions) { return getFiles(new Stage(), extensions); }
 
-    public static File[] getPNGs(Stage stage) {
-        return getFiles(stage, PNG);
-    }
 
-    public static File getJPG(Stage stage) {
-        return getFile(stage, JPG);
-    }
+    public static File getPNG(Stage stage) { return getFile(stage, PNG); }
+    public static File[] getPNGs(Stage stage) { return getFiles(stage, PNG); }
+    public static File getPNG() { return getFile(PNG); }
+    public static File[] getPNGs() { return getFiles(PNG); }
 
-    public static File[] getJPGs(Stage stage) {
-        return getFiles(stage, JPG);
-    }
 
-    public static File getICO(Stage stage) {
-        return getFile(stage, ICO);
-    }
+    public static File getJPG(Stage stage) { return getFile(stage, JPG); }
+    public static File[] getJPGs(Stage stage) { return getFiles(stage, JPG); }
+    public static File getJPG() { return getFile(JPG); }
+    public static File[] getJPGs() { return getFiles(JPG); }
 
-    public static File[] getICOs(Stage stage) {
-        return getFiles(stage, ICO);
-    }
 
-    public static File getGIF(Stage stage) {
-        return getFile(stage, GIF);
-    }
+    public static File getICO(Stage stage) { return getFile(stage, ICO); }
+    public static File[] getICOs(Stage stage) { return getFiles(stage, ICO); }
+    public static File getICO() { return getFile(ICO); }
+    public static File[] getICOs() { return getFiles(ICO); }
 
-    public static File[] getGIFs(Stage stage) {
-        return getFiles(stage, GIF);
-    }
 
-    public static File getJFIF(Stage stage) {
-        return getFile(stage, JFIF);
-    }
+    public static File getGIF(Stage stage) { return getFile(stage, GIF); }
+    public static File[] getGIFs(Stage stage) { return getFiles(stage, GIF); }
+    public static File getGIF() { return getFile(GIF); }
+    public static File[] getGIFs() { return getFiles(GIF); }
 
-    public static File[] getJFIFs(Stage stage) {
-        return getFiles(stage, JFIF);
-    }
+
+    public static File getJFIF(Stage stage) { return getFile(stage, JFIF); }
+    public static File[] getJFIFs(Stage stage) { return getFiles(stage, JFIF); }
+    public static File getJFIF() { return getFile(JFIF); }
+    public static File[] getJFIFs() { return getFiles(JFIF); }
+
 
     public static File getPDF(Stage stage) { return getFile(stage, PDF); }
     public static File[] getPDFs(Stage stage) { return getFiles(stage, PDF);}
+    public static File getPDF() { return getFile(PDF); }
+    public static File[] getPDFs() { return getFiles(PDF);}
 
 
     public static File getHTML(Stage stage) { return getFile(stage, HTML); }
     public static File[] getHTMLs(Stage stage) { return getFiles(stage, HTML);}
+    public static File getHTML() { return getFile(HTML); }
+    public static File[] getHTMLs() { return getFiles(HTML);}
 
 
     public static File getXHTML(Stage stage) { return getFile(stage, XHTML); }
     public static File[] getXHTMLs(Stage stage) { return getFiles(stage, XHTML);}
+    public static File getXHTML() { return getFile(XHTML); }
+    public static File[] getXHTMLs() { return getFiles(XHTML);}
 
 
     public static File getPHTML(Stage stage) { return getFile(stage, PHTML); }
     public static File[] getPHTMLs(Stage stage) { return getFiles(stage, PHTML);}
+    public static File getPHTML() { return getFile(PHTML); }
+    public static File[] getPHTMLs() { return getFiles(PHTML);}
 
 
     public static File getXML(Stage stage) { return getFile(stage, XML); }
     public static File[] getXMLs(Stage stage) { return getFiles(stage, XML);}
+    public static File getXML() { return getFile(XML); }
+    public static File[] getXMLs() { return getFiles(XML);}
+
+
+    public static File getFXML(Stage stage) { return getFile(stage, FXML); }
+    public static File[] getFXMLs(Stage stage) { return getFiles(stage, FXML);}
+    public static File getFXML() { return getFile(FXML); }
+    public static File[] getFXMLs() { return getFiles(FXML);}
 
 
     public static File getPHP(Stage stage) { return getFile(stage, PHP); }
@@ -273,6 +267,10 @@ public class File extends java.io.File {
     public static File[] getCPPs(Stage stage) { return getFiles(stage, CPP);}
 
 
+    public static File getCSHARP(Stage stage) { return getFile(stage, CSHARP); }
+    public static File[] getCSHARPs(Stage stage) { return getFiles(stage, CSHARP);}
+
+
     public static File getINO(Stage stage) { return getFile(stage, INO); }
     public static File[] getINOs(Stage stage) { return getFiles(stage, INO);}
 
@@ -316,13 +314,100 @@ public class File extends java.io.File {
     public static File getSQL(Stage stage) { return getFile(stage, SQL); }
     public static File[] getSQLs(Stage stage) { return getFiles(stage, SQL);}
 
-    public static File getDirectory(Stage stage) {
-        return convert(dc.showDialog(stage))[0];
-    }
+    public static File getPHP() { return getFile(PHP); }
+    public static File[] getPHPs() { return getFiles(PHP);}
 
-    public boolean hasNext() {
-        return in.hasNext();
-    }
+
+    public static File getCSS() { return getFile(CSS); }
+    public static File[] getCSSs() { return getFiles(CSS);}
+
+
+    public static File getJS() { return getFile(JS); }
+    public static File[] getJSs() { return getFiles(JS);}
+
+
+    public static File getJSON() { return getFile(JSON); }
+    public static File[] getJSONs() { return getFiles(JSON);}
+
+
+    public static File getJAVA() { return getFile(JAVA); }
+    public static File[] getJAVAs() { return getFiles(JAVA);}
+
+
+    public static File getKOTLIN() { return getFile(KOTLIN); }
+    public static File[] getKOTLINs() { return getFiles(KOTLIN);}
+
+
+    public static File getSCALA() { return getFile(SCALA); }
+    public static File[] getSCALAs() { return getFiles(SCALA);}
+
+
+    public static File getPYTHON() { return getFile(PYTHON); }
+    public static File[] getPYTHONs() { return getFiles(PYTHON);}
+
+
+    public static File getRUBY() { return getFile(RUBY); }
+    public static File[] getRUBYs() { return getFiles(RUBY);}
+
+
+    public static File getC() { return getFile(C); }
+    public static File[] getCs() { return getFiles(C);}
+
+
+    public static File getCPP() { return getFile(CPP); }
+    public static File[] getCPPs() { return getFiles(CPP);}
+
+
+    public static File getCSHARP() { return getFile(CSHARP); }
+    public static File[] getCSHARPs() { return getFiles(CSHARP);}
+
+
+    public static File getINO() { return getFile(INO); }
+    public static File[] getINOs() { return getFiles(INO);}
+
+
+    public static File getVB() { return getFile(VB); }
+    public static File[] getVBs() { return getFiles(VB);}
+
+
+    public static File getGO() { return getFile(GO); }
+    public static File[] getGOs() { return getFiles(GO);}
+
+
+    public static File getRUST() { return getFile(RUST); }
+    public static File[] getRUSTs() { return getFiles(RUST);}
+
+
+    public static File getSWIFT() { return getFile(SWIFT); }
+    public static File[] getSWIFTs() { return getFiles(SWIFT);}
+
+
+    public static File getTEXT() { return getFile(TEXT); }
+    public static File[] getTEXTs() { return getFiles(TEXT);}
+
+
+    public static File getMD() { return getFile(MD); }
+    public static File[] getMDs() { return getFiles(MD);}
+
+
+    public static File getREADME() { return getFile(README); }
+    public static File[] getREADMEs() { return getFiles(README);}
+
+
+    public static File getBIBTEX() { return getFile(BIBTEX); }
+    public static File[] getBIBTEXs() { return getFiles(BIBTEX);}
+
+
+    public static File getLATEX() { return getFile(LATEX); }
+    public static File[] getLATEXs() { return getFiles(LATEX);}
+
+
+    public static File getSQL() { return getFile(SQL); }
+    public static File[] getSQLs() { return getFiles(SQL);}
+
+    public static File getDirectory(Stage stage) { return convert(dc.showDialog(stage))[0]; }
+
+    public boolean hasNext() { return in.hasNext(); }
 
     public int count(String substring) {
         String input = read();
@@ -330,21 +415,13 @@ public class File extends java.io.File {
         return words.length-1;
     }
 
-    public int count(char substring) {
-        return count(substring+"");
-    }
+    public int count(char substring) { return count(substring+""); }
 
-    public int count(int substring) {
-        return count(substring+"");
-    }
+    public int count(int substring) { return count(substring+""); }
 
-    public int count(double substring) {
-        return count(substring+"");
-    }
+    public int count(double substring) { return count(substring+""); }
 
-    public int count(float substring) {
-        return count(substring+"");
-    }
+    public int count(float substring) { return count(substring+""); }
 
     public String[] split(String delimeter) {
         String input = read();
@@ -383,49 +460,9 @@ public class File extends java.io.File {
         else return "";
     }
 
-
-
     public String[] readLines() {
         if (in != null) return read().split("\n");
         else return new String[0];
-    }
-
-    public int[] readInts(String delimeter) {
-        String[] s;
-        if (delimeter == "\n") s = readLines();
-        else s = readLine().split(delimeter);
-        int[] rearr = new int[s.length];
-        for(int i = 0; i < s.length; i++) {
-            rearr[i] = Integer.parseInt(s[i].trim());
-        }
-        return rearr;
-    }
-    public int[] readInts(char delimeter) throws IOException {
-        try {
-            String[] s;
-            if (delimeter == '\n') s = readLines();
-            else s = readLine().split("" + delimeter);
-            int[] rearr = new int[s.length];
-            for (int i = 0; i < s.length; i++) {
-                rearr[i] = Integer.parseInt(s[i].trim());
-            }
-            return rearr;
-        } catch (Exception ex) {
-            throw new IOException();
-        }
-    }
-    public int[] readInts() throws IOException {
-        try {
-            String[] s = readLines();
-            int[] rearr = new int[s.length];
-            for (int i = 0; i < s.length; i++) {
-                rearr[i] = Integer.parseInt(s[i].trim());
-            }
-            return rearr;
-        }
-        catch (Exception ex) {
-            throw new IOException();
-        }
     }
 
     public void close() {
@@ -433,5 +470,23 @@ public class File extends java.io.File {
         try { out.close(); } catch(NullPointerException ex) {}
     }
 
-    public URI relativize(File file) { return toURI().relativize(file.toURI()); }
+    public String relativize(File file) { return toURI().relativize(file.toURI()).getPath(); }
+
+    @Override
+    public boolean equals(Object obj) {
+        System.out.println("equals(Object) called");
+        if (obj == this) return true;
+        if(obj == null) return false;
+        if (obj instanceof File) {
+            File file = (File) obj;
+            return getAbsolutePath().equals(file.getAbsolutePath());
+        }
+        return false;
+    }
+
+    public boolean equals(File other) { return getAbsolutePath().equals(other.getAbsolutePath()); }
+
+    public File clone() { return new File(getAbsolutePath()); }
+
+    public int compareTo(File o) { return super.compareTo(o); }
 }
